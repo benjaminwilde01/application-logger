8e1bb82b5f32248220f7c2c67ed00c40fa061220
Benjamin Wilde <bendavidwilde95@gmail.com>
2021-05-28 10:23:20 -0500

    testing

--- controllers/job-applications.js
+++ controllers/job-applications.js
@@ -8,18 +8,16 @@ const Application = require('../models/application.js');
 // ROUTES
 // Index
 router.get('/', (req, res) => {
-//     Application.find({}, (err, allApplications) => {
-//         if (err) {
-//             res.send(err)
-//         } else {
-//             res.render('index.ejs', {
-//                 applications: allApplications
-//             });
-//         };
-//     });
-// });
-    res.send('g')
-})
+    Application.find({}, (err, allApplications) => {
+        if (err) {
+            res.send(err)
+        } else {
+            res.render('index.ejs', {
+                applications: allApplications
+            });
+        };
+    });
+});
 
 // NEW
 router.get('/new', (req, res) => {
